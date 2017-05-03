0e15be9140b0cc7457bd21b24cb58033bac5ff44
Euan Mackintosh <student@macbook.local>
2017-05-03 10:48:28 -0400

    11 finished again

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -26,17 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
     @IBAction func onePressed(_ sender: Any) {
         model.addToNewValue(digit: "1")
         labelDisplay.text = model.providedValue
     }
 
+    
     @IBAction func twoPressed(_ sender: Any) {
         model.addToNewValue(digit: "2")
         labelDisplay.text = model.providedValue
     }
-
+    
     @IBAction func threePressed(_ sender: Any) {
         model.addToNewValue(digit: "3")
         labelDisplay.text = model.providedValue
